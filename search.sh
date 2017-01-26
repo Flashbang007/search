0d05b3d9c73df1d2cd0a6b52eec8b1bca2b5a262
Flashbang007 <Flashbang007@gmail.com>
2017-01-26 11:58:04 +0100

    Create search.sh

---
+++ search.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# This is for easy searching files
+WHICH="/usr/bin/which"
+FIND="/usr/bin/find"
+PWD="/bin/pwd"
+SEARCHPATTERN=${@:$#}
+SEARCHPATH=$($PWD)
+OPTION="-name"
+
+#Colors#
+CRESTORE='\033[0m'
+CRED='\033[00;31m'
+######################
+
+print_help() {
+echo -e "
+Usage: search.sh [OPTION] [SEARCHPATTERN]
+
+        -h      show this help
+        -p      [SEARCHPATH] search only this Path
+        -e      Search exactly this Filename
+        -s      Search for the Path of a Programm or Script (like which)
+"
+}
+
+############ SEARCH MODES #################
+exact_search() {
+$FIND $SEARCHPATH $OPTION $SEARCHPATTERN
+}
+
+script_search() {
+
+$WHICH $SEARCHPATTERN
+}
+
+find_search() {
+
+$FIND $SEARCHPATH $OPTION "*$SEARCHPATTERN*"
+}
+
+if [[ -z $SEARCHPATTERN ]]; then
+        print_help
+        exit 1
+fi
+###########################################
+
+############## OPTIONS ####################
+while getopts 'hp:esn' opt
+do
+        case $opt in
+                h)
+                        print_help
+                        exit 0
+                        ;;
+                p)
+                        SEARCHPATH="$OPTARG"
+                        ;;
+                e)
+                        MODE="exact"
+                        ;;
+                s)
+                        MODE="script"
+                        ;;
+                *)
+                        find_search
+                        exit 0
+                        ;;
+        esac
+done
+##########################################
+
+if [[ -z $MODE ]]; then
+        find_search
+        exit 0
+elif [[ $MODE -eq "exact" ]];then
+        exact_search
+        exit 0
+elif [[ $MODE -eq "script"  ]];then
+        script_search
+        exit 0
+fi
+
+
+exit 10
